9072e14ad663a974bf3260cb226aac11d273f3e8
hailey park <blessedgene11@gmail.com>
2020-09-01 20:32:58 +0900

    feat : signup_style done

--- frontend/src/pages/Join.tsx
+++ frontend/src/pages/Join.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import appStoreDownload from '../resource/image/appStoreDownload.png';
 import googleStoreDownload from '../resource/image/googleStoreDownload.png';
-import Logo from '../components/Logo';
+import { StyledH1 } from '../components/Logo';
 
 function Join() {
   const appStoreUrl = 'https://apps.apple.com/app/instagram/id389801252?vt=lo';
@@ -14,15 +14,81 @@ function Join() {
     margin: 0 auto;
   `;
   const JoinWrapper = styled.div`
+    width: 100%;
     height: 542px;
     background: #ffffff;
     border: 1px solid #dbdbdb;
     margin: 30px 0 10px 0;
+    padding: 0 40px;
+    box-sizing: border-box;
 
-    Logo {
+    .policy {
+      color: #828282;
+      padding: 20px 10px 10px;
+      display: block;
+      text-align: center;
     }
   `;
-  const coinstagramLogo = styled.h1``;
+  const Header = styled.div`
+    height: 150px;
+  `;
+  const MainLogo = styled(StyledH1)`
+    font-size: 50px;
+    text-align: center;
+    margin-top: 30px;
+    font-weight: 600;
+  `;
+  const StyledH2 = styled.h2`
+    color: #828282;
+    font-size: 1.2rem;
+    text-align: center;
+    margin: 10px 0;
+  `;
+  const OrBorder = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 10px 0 20px;
+
+    .leftLine,
+    .rightLine {
+      height: 1px;
+      background: #dbdbdb;
+      
+      width: 100%;
+      position: relative;
+    top: 0.65em;
+}
+    }
+    .or {
+      margin: 0 18px;
+      color: #828282;
+      font-weight : bold;
+      white-space : nowrap;
+      font-size: .9em;
+    }
+  `;
+
+  const Join = styled.div`
+    input {
+      background: #fafafa;
+      color: #828282;
+      border: 1px solid #dbdbdb;
+      padding: 10px;
+      margin-bottom: 10px;
+      border-radius: 3px;
+    }
+  `;
+  const JoinButton = styled.button`
+    display: block;
+    background: #b2dffc;
+    color: white;
+    font-weight: bold;
+    text-align: center;
+    width: 100%;
+    height: 30px;
+    border-radius: 3px;
+  `;
+
   const AskLoginWrapper = styled.div`
     height: 70px;
     background: #ffffff;
@@ -56,7 +122,42 @@ function Join() {
   return (
     <Wrapper>
       <JoinWrapper>
-        <Logo />
+        <Header>
+          <MainLogo>coInstagram</MainLogo>
+
+          <StyledH2>친구들의 사진과 동영상을 보려면 가입하세요.</StyledH2>
+        </Header>
+        <OrBorder>
+          <div className="leftLine"></div>
+          <div className="or">또는</div>
+          <div className="rightLine"></div>
+        </OrBorder>
+        <Join>
+          <input
+            type="text"
+            name="phoneNumber"
+            placeholder="휴대폰 또는 이메일 주소"
+            size={34}
+          />
+          <input type="text" name="userName" placeholder="성명" size={34} />
+          <input
+            type="text"
+            name="userId"
+            placeholder="사용자 이름"
+            size={34}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호"
+            size={34}
+          />
+        </Join>
+        <JoinButton type="submit">가입</JoinButton>
+        <small className="policy">
+          가입하면 Instagram의 <b>약관, 데이터 정책</b> 및 <b>쿠키 정책</b>에
+          동의하게 됩니다.
+        </small>
       </JoinWrapper>
       <AskLoginWrapper>
         {/* 로그인 페이지 라우팅 구현 필요 */}
