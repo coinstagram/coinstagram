b0081b639634cfac588b3725d2e066b706c93138
ghkdxogus <clowns1232@gmail.com>
2020-09-11 15:45:43 +0900

    dev: backend API Make post_like comment_like bookmark

--- frontend/src/components/upload/UploadHeader.tsx
+++ frontend/src/components/upload/UploadHeader.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-  line-height: 60px;
-  font-size: 16px;
-  font-weight: bold;
-  padding-left: 15px;
-  border-bottom: 1px solid rgb(219, 219, 219);
-`;
+import { StyledDiv } from './UploadHeader.style';
 
 function UploadHeader() {
-  return <StyledDiv>새 게시물 작성1</StyledDiv>;
+  return <StyledDiv>새 게시물 작성</StyledDiv>;
 }
 
 export default UploadHeader;
