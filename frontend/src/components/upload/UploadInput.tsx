e320bcf535a95f99313f7bd306e7cbfa8300ffc1
Hwang taehyeon <clown1232@gmail.com>
2020-09-16 21:37:47 +0800

    bug: image add, add, add end

--- frontend/src/components/upload/UploadInput.tsx
+++ frontend/src/components/upload/UploadInput.tsx
@@ -13,20 +13,14 @@ interface resDataProps {
 
 const UploadInput: React.FC<UploadDetailsProps> = ({ image }) => {
   const [imageURL, setImageURL] = useState<Array<string>>([]);
-  const [imageFile, setImageFile] = useState<FileList>();
+  const [imageFile, setImageFile] = useState<Array<String>>([]);
 
   const isSelectedImg = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files === null) {
-      setImageFile(() => event.target.files);
-    } else {
-    }
-
     const FileUrl = await uploadService.uploadImageView(
       event.target.files,
       localStorage.getItem('access_token'),
     );
-
-    image(FileUrl);
+    setImageFile([...imageFile, ...FileUrl]);
 
     setImageURL([
       ...imageURL,
@@ -35,9 +29,8 @@ const UploadInput: React.FC<UploadDetailsProps> = ({ image }) => {
   };
 
   React.useEffect(() => {
-    console.log(imageFile);
-    // console.log('imageFile', imageFile);
-  }, [imageFile]);
+    image(imageFile);
+  }, [image, imageFile]);
 
   return (
     <>
