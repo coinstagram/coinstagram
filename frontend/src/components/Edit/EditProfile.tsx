4bdf2b937ab242937d763a1ff529f11ae2d5fb41
LEE-WOO-SEONG <akakqogk@gmail.com>
2020-09-17 21:47:50 +0900

    config: working update for merge

---
+++ frontend/src/components/Edit/EditProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Thumbnail from '../common/Thumbnail';
+
+function EditProfile() {
+  return (
+    <div>
+      <Thumbnail size={50} imageUrl={null} />
+    </div>
+  );
+}
+
+export default EditProfile;
