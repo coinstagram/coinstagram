24e023af6b889d93b9877847f23e092e02afbec7
hailey park <blessedgene11@gmail.com>
2020-09-22 12:21:12 +0900

    update

--- backend/app.js
+++ backend/app.js
@@ -7,21 +7,22 @@ const signinRouter = require('./router/signin');
 const loginRouter = require('./router/login');
 const postRouter = require('./router/post');
 const userRouter = require('./router/user');
-
 const bodyParser = require('body-parser');
-
 const app = express();
+app.use(function (req, res, next) {
+  const check = /\/api/g;
+  req.url = req.url.replace(check, '');
+  next();
+});
 // eslint-disable-next-line no-undef
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(userRouter);
 app.use(signinRouter);
 app.use(loginRouter);
 app.use(postRouter);
 app.use('/uploads', express.static('uploads'));
-
 app.listen(PORT, () => {
   console.log('서버 실행중');
 });
