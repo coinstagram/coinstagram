aa7a9a3b712b1b70a8673a5b6159ec5493c3cdc0
hailey park <blessedgene11@gmail.com>
2020-10-01 11:42:14 +0900

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   port: '3306',
   user: 'root',
-<<<<<<< HEAD
   password: 'mysql0909',
-=======
-  password: 'a70113',
->>>>>>> 16c9b2181fe3ea0d279a855d8c5243b6c6491ce6
   database: 'coinstagram',
   connectionLimit: 10,
   queueLimit: 0,
