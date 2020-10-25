251cd38fe3d77a90ab8dccf49342c00742f15eb1
LEE-WOO-SEONG <akakqogk@gmail.com>
2020-10-25 15:59:37 +0900

    feat: correct useObserver hook & feed / random component

--- frontend/src/containers/RandomPostsContainer.tsx
+++ frontend/src/containers/RandomPostsContainer.tsx
@@ -1,14 +1,16 @@
 import React, { useCallback, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getRandomPostsSaga, resetRandomPost } from '../redux/modules/post';
 import { getPostCountsSaga } from '../redux/modules/otherPost';
 import { resetMyComment } from '../redux/modules/comment';
+import RootState from '../type';
 
 // components
 import RandomPosts from '../components/explore/RandomPosts';
 
 function RandomPostsContainer() {
   const dispatch = useDispatch();
+  const { loading, error, randomPosts, isLast } = useSelector((state: RootState) => state.posts.randomPosts);
 
   useEffect(() => {
     dispatch(resetMyComment());
@@ -32,7 +34,7 @@ function RandomPostsContainer() {
     [dispatch],
   );
 
-  return <RandomPosts getRandomPosts={getRandomPosts} getPostCounts={getPostCounts} />;
+  return <RandomPosts loading={loading} error={error} randomPosts={randomPosts} isLast={isLast} getPostCounts={getPostCounts} />;
 }
 
 export default RandomPostsContainer;
