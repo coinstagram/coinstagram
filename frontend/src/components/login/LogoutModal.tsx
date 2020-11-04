bcb51618bec170f8ca70aa95e93ec3c80d4c54fb
hailey park <blessedgene11@gmail.com>
2020-11-04 10:18:43 +0900

    feat:logoutmodal_done

--- frontend/src/components/login/LogoutModal.tsx
+++ frontend/src/components/login/LogoutModal.tsx
@@ -1,41 +1,36 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../redux/modules/auth';
 import TokenService from '../../redux/services/tokenService';
 import useWindowWidth from '../../hooks/useWindowWidth';
 import { StyledBg, StyledModal } from './LogoutModalStyle';
-import Home from '../../pages/Home';
 
 interface logoutModalProps {
-  logoutModal: boolean;
+  closeModal?: () => void;
 }
-export default function LogoutModal() {
+export default function LogoutModal({ closeModal }: logoutModalProps) {
   const width = useWindowWidth();
   const dispatch = useDispatch();
-  const [loggedIn, setLoggedIn] = useState(false);
   const yesLogout = () => {
     dispatch(logout());
     TokenService.remove();
   };
-  const noLogout = () => {
-    setLoggedIn(true);
-  };
 
   return (
-    <StyledBg>
-      <StyledModal width={width}>
-        <p>정말 로그아웃하시겠습니까?</p>
-        <div className="btns">
-          <button className="yesBtn" onClick={yesLogout}>
-            네
-          </button>
-          <button className="noBtn" onClick={noLogout}>
-            아니오
-            {/* {loggedIn && <Route path="/" component={Home} />} */}
-          </button>
-        </div>
-      </StyledModal>
-    </StyledBg>
+    <>
+      <StyledBg>
+        <StyledModal width={width}>
+          <p>정말 로그아웃하시겠습니까?</p>
+          <div className="btns">
+            <button className="yesBtn" onClick={yesLogout}>
+              네
+            </button>
+            <button className="noBtn" onClick={closeModal}>
+              아니오
+            </button>
+          </div>
+        </StyledModal>
+      </StyledBg>
+    </>
   );
 }
