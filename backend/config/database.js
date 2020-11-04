afd15414322ec167e960336c6e14b2dbf83b1e44
LEE-WOO-SEONG <akakqogk@gmail.com>
2020-11-04 19:53:27 +0900

    feat: update database pw, and upload images

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   port: '3306',
   user: 'root',
-<<<<<<< HEAD
   password: 'g1560318',
-=======
-  password: 'mysql0909',
->>>>>>> 8edda8e177ad65542db9c74857178585277c8274
   database: 'coinstagram',
   connectionLimit: 10,
   queueLimit: 0,
